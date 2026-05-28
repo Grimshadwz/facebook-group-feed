6d3dd134620b5a5cd9f5d4a5c6d63f7c350d03c4
Paradox Studios <78919950+Grimshadwz@users.noreply.github.com>
2026-05-28 18:47:38 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,124 +1,31 @@
-import concurrent.futures
-import logging
 import os
 import time
-from datetime import datetime
-from typing import Any, Callable, Iterable, Mapping
-
 from dotenv import load_dotenv
 from facebook_scraper import get_posts
-
 import channel
-
-fb_logger = logging.getLogger("facebook-scraper")
-fb_logger.disabled = True
-logger = logging.getLogger("facebook-group-feed-bot")
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(handler)
 
 load_dotenv()
 
-FACEBOOK_GROUP_IDS: list[str] = os.getenv("FACEBOOK_GROUP_IDS", "").split(",")
-SENT_POSTS_FILENAME: str = os.getenv("SENT_POSTS_FILENAME", "sent_posts.txt")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+FACEBOOK_ID = os.getenv("FACEBOOK_ID")
 
-CHANNEL: channel.Channel = channel.DiscordWebhookChannel(os.getenv("WEBHOOK_URL", ""))
+# Initialize the Discord Webhook channel
+CH = channel.DiscordWebhookChannel(WEBHOOK_URL)
 
+print("Checking for new messages...")
 
-def process_posts_in_group(group: str | int, callback: Callable[[Mapping[str, Any]], None]) -> None:
-    """Fetches posts from a group and calls the callback function for each post
+# Fetch the most recent post
+try:
+    for post in get_posts(FACEBOOK_ID, pages=1):
+        post_text = post.get('text', '')
+        post_url = post.get('post_url', '')
+        
+        if post_url:
+            message = f"Check out my latest Facebook post: {post_url}"
+            CH.send_message(message)
+            print("Post sent to Discord successfully!")
+            break # Only send the single newest post
+except Exception as e:
+    print(f"Error fetching Facebook posts: {e}")
 
-    Args:
-        group: The group id
-    """
-    for post in get_posts(group=group, pages=2, options={"comments": 0, "progress": True}):
-        callback(post)
-
-
-def get_sent_post_ids() -> set[str]:
-    """Get the ids of the posts we've already sent
-
-    Returns:
-        A set of post ids
-    """
-    sent_posts = set()
-    if not os.path.exists(SENT_POSTS_FILENAME):
-        open(SENT_POSTS_FILENAME, "w+").close()
-    with open(SENT_POSTS_FILENAME, "r") as f:
-        for line in f:
-            sent_posts.add(line.strip())
-    return sent_posts
-
-
-def write_sent_post_ids(sent_posts: set[str]) -> None:
-    """Write the ids of the posts we've already sent
-
-    Args:
-        sent_posts: A set of post ids
-    """
-    with open(SENT_POSTS_FILENAME, "a") as f:
-        for post_id in sent_posts:
-            f.write(post_id + "\n")
-
-
-def send_message(message: Mapping[str, Any]) -> None:
-    """Send a message to the Discord webhook and write the post id to the sent posts file
-
-    Args:
-        message: The message to send (as returned by facebook_scraper.get_posts)
-    """
-    logger.info("Sending message: " + message["post_url"])
-    CHANNEL.send(message)
-    write_sent_post_ids({message["post_id"]})
-
-
-def send_test_message() -> None:
-    """Send a test message to the Discord webhook"""
-    send_message(
-        {
-            "post_id": "43284432493284792374",
-            "post_text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl aliquam nunc, eget ultricies.",
-            "images_lowquality": ["https://via.placeholder.com/640x360"],
-            "post_url": "https://m.facebook.com/groups/groupname/permalink/5839808520835092/",
-            "header": "Jonah Lawrence\u200fGroup Name",
-            "time": datetime.now(),
-            "timestamp": 1621234567,
-            "username": "Jonah Lawrence",
-        }
-    )
-
-
-def send_all_new_messages(groups: Iterable[str | int]) -> None:
-    """Send all new messages from the given groups to the Discord webhook
-
-    Args:
-        groups: A list of group identifiers
-    """
-    sent_posts = get_sent_post_ids()
-
-    def send_post_callback(post: Mapping[str, Any]) -> None:
-        """Send a post to the Discord webhook if it's new"""
-        # if the post is older than 2 hours, don't send it (compare unix timestamps)
-        if post["timestamp"] < time.time() - 60 * 60 * 2:
-            return
-        # if the post is already sent, don't send it again
-        if post["post_id"] in sent_posts:
-            return
-        # otherwise, send it
-        send_message(post)
-        # add the post id to the set of sent posts
-        sent_posts.add(post["post_id"])
-
-    # create a thread pool to process the groups in parallel
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for group in groups:
-            executor.submit(process_posts_in_group, group, send_post_callback)
-
-
-if __name__ == "__main__":
-    logger.info(f"Checking for new messages in groups: {', '.join(FACEBOOK_GROUP_IDS)}")
-    while True:
-        send_all_new_messages(FACEBOOK_GROUP_IDS)
-        logger.info("Checking again in 5 minutes...")
-        time.sleep(60 * 5)  # sleep for 5 minutes before checking again
+print("Check complete. Closing script.")
